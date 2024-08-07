2c28ab52cacc8861c1aaf413baf872a3e9a5b39a
Sushant Agrawal <capitalch@gmail.com>
2024-08-08 01:02:59 +0530

    init

--- crmls-lab-app/src/App.tsx
+++ crmls-lab-app/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Compliance } from './components/compliance'
+import { HotFix1 } from './components/hotFix1'
 import { Membership } from './components/membership'
 function App() {
   return (
     <div>
       <Compliance />
       <Membership />
+      <HotFix1 />
     </div>
   )
 }
---
+++ crmls-lab-app/src/components/hotFix1.tsx
@@ -0,0 +1,7 @@
+export function HotFix1() {
+  return (
+    <div>
+      <button>HotFix1</button>
+    </div>
+  )
+}
