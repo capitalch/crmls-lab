7048d1a0487d3c0231d60e1d286fbe2be8fe7fdf
Sushant Agrawal <capitalch@gmail.com>
2024-08-08 14:19:24 +0530

    hotfix2 branch added hotfix2Comp

--- crmls-lab-app/src/App.tsx
+++ crmls-lab-app/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Compliance } from './components/compliance'
 import { HotFix1 } from './components/hotFix1'
+import { HotFix2 } from './components/hotfix2Comp'
 import { Members } from './components/members'
 import { Membership } from './components/membership'
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Membership />
       <HotFix1 />
       <Members />
+      <HotFix2 />
     </div>
   )
 }
---
+++ crmls-lab-app/src/components/hotfix2Comp.tsx
@@ -0,0 +1,5 @@
+export function HotFix2() {
+    return (<div>
+        <button>HotFix2</button>
+    </div>)
+}
